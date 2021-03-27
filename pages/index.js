fc9e918c180b72ecc36b6f4f781cb968c323eabd
Jon Gelsey <github@gelsey.com>
2021-03-27 15:25:36 -0700

    test

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by XXXXediting{' '}
+          Get started by Yediting{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
